2df1e0dcdc76c500ce5304df5d261b01928aecb6
Hasan Ul Kabir Utsho <mcutsho96@gmail.com>
2025-12-11 11:43:39 +0600

    get jobs api

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
-
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 // middleware
 app.use(cors());
 app.use(express.json());
-
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dofzwuy.mongodb.net/?appName=Cluster0`;
 
@@ -21,16 +17,28 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const jobsCollection = client.db("careerCode").collection("jobs");
+
+    // jobs Api
+    app.get("/jobs", async (req, res) => {
+      const cursor = jobsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -38,11 +46,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("career code is cooking");
+});
 
-app.get('/', (req,res)=>{
-    res.send('career code is cooking')
-})
-
-app.listen(port, ()=>{
-    console.log(`career code server is running on port: ${port}`);
+app.listen(port, () => {
+  console.log(`career code server is running on port: ${port}`);
 });
