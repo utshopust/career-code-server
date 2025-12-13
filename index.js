1d49b88d683a22bc53d206cfac9d1f6647c52431
Hasan Ul Kabir Utsho <mcutsho96@gmail.com>
2025-12-13 21:37:12 +0600

    add job application related apis

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ async function run() {
     await client.connect();
 
     const jobsCollection = client.db("careerCode").collection("jobs");
+    const applicationsCollection = client.db("careerCode").collection("applications");
 
     // jobs Api
     app.get("/jobs", async (req, res) => {
@@ -38,6 +39,14 @@ async function run() {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await jobsCollection.findOne(query);
+      res.send(result);
+    });
+
+    // job application related apis
+    app.post("/applications", async (req, res) => {
+      const application = req.body;
+      console.log(application);
+      const result = await applicationsCollection.insertOne(application);
       res.send(result);
     });
 
